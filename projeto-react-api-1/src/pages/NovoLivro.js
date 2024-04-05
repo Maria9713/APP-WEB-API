078417dffa3c492346409239a649f33bc9532613
Maria9713 <mvtfernandes2outlook.com>
2024-04-04 21:29:17 -0300

    Acesso a API JSON_SERVER

--- projeto-react-api-1/src/pages/NovoLivro.js
+++ projeto-react-api-1/src/pages/NovoLivro.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+
 import Input from '../components/form/Input';
 import styles from './NovoLivro.module.css';
+import Select from '../components/form/Select';
 
 function NovoLivro () {
+
+    const [categories, setCategories] = useState([]);
+    useEffect ( ()=>{
+        fetch(
+            'http://localhost:5000/categories',
+            {
+            method:'GET',
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+            }).then(
+                (resp)=>resp.json()
+            ).then(
+                (data)=>{
+                    setCategories(data);
+                    console.log(data);
+                }
+            ).catch(
+                (error)=>{
+                    console.log(error);
+                }
+            )
+            
+        }, [])
+
     return(
 
         <section className={styles.novo_livro_container}>
@@ -13,34 +41,43 @@ function NovoLivro () {
                 {/* <p>
                     <input type='text' placeholder='Nome do livro' />
                 </p> */}
+
                 <Input
-                type='text'
-                name='nome_livro'
-                id='nome_livro'
-                placeholder='Digite o Título do Livro'
-                text='Digite o Título do Livro:'
+                    type='text'
+                    name='nome_livro'
+                    id='nome_livro'
+                    placeholder='Digite o Título do Livro'
+                    text='Digite o Título do Livro:'
                 />
 
                 {/* <p>
                     <input type='text' placeholder='Nome do autor' />
                 </p> */}
+
                 <Input
-                type='text'
-                name='nome_autor'
-                id='nome_autor'
-                placeholder='Digite o Nome do Autor'
-                text='Digite o Nome do Autor:'
+                    type='text'
+                    name='nome_autor'
+                    id='nome_autor'
+                    placeholder='Digite o Nome do Autor'
+                    text='Digite o Nome do Autor:'
                 />
 
                 {/* <p>
                     <input type='text' placeholder='Descrição do livro' />
                 </p> */}
+                
                 <Input
-                type='text'
-                name='descrição'
-                id='descrição'
-                placeholder='Digite uma descrição para o livro'
-                text='Descrição:'
+                    type='text'
+                    name='descrição'
+                    id='descrição'
+                    placeholder='Digite uma descrição para o livro'
+                    text='Descrição:'
+                />
+
+                <Select
+                    name="categoria_id"
+                    text="Selecione a categoria do livro"
+                    options={categories}
                 />
 
                 <p>
